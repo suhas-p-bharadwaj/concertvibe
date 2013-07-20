ed975bb558b3fa2793cd53ec927d278046c1fe3a
Will Moritz <will@crowdfavorite.com>
2013-07-20 17:54:56 -0600

    better header

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=7,IE=9">   
+  <meta http-equiv="X-UA-Compatible" content="IE=7,IE=9">
   <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
 
   <title>ConcertVibe</title>
@@ -14,65 +14,47 @@
   <script src='http://api.tiles.mapbox.com/mapbox.js/v1.3.0/mapbox.js'></script>
   <link href='http://api.tiles.mapbox.com/mapbox.js/v1.3.0/mapbox.css' rel='stylesheet' />
   <!--[if lte IE 8]>
-    <link href='http://api.tiles.mapbox.com/mapbox.js/v1.3.0/mapbox.ie.css' rel='stylesheet' >
-  <![endif]-->
-  
-</head>
+    <link href='httxp://api.tiles.mapbox.com/mapbox.js/v1.3.0/mapbox.ie.css' rel='stylesheet' >
+    <![endif]-->
+
+  </head>
   <body>
-    
+
     <div class="container-fluid">
       <div class="row-fluid">
-        
-        <div class="span10">
-          
           <!--Nav content-->
-          <div class="header-bar" id="header">
-            <div class="container clearfix">
-            <div class="full-col">
-            <h1>
-              <a href="/"><div class="white" id="logo">ConcertVibe&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div></a>
-            </h1>
+            <div class="header-bar" id="header">
+              <div class="header-container">
+                <h1 class="span2">
+                  <a href="/" class="logo">ConcertVibe</a>
+                </h1>
+                <!--Search content-->
+                <div id="search-box" class="span8" >
+                  <div id="dialog">
+                    <input id="q" name="q" placeholder="Enter a band" type="text" value="Phish">
+                    <input id="search-btn" class="search btn primary" name="" type="submit" value="Go">
+                  </div>
+                </div>
 
-            <nav id="main-menu">
-            <ul class="clearfix">
-                
-            </ul>
-            </nav>
-            <nav id="secondary-menu">
-            <ul class="clearfix">
-              <li><a href="/" onclick="if ($('#q').is(':visible')) { $('#search-box').slideUp('fast'); } else { $('#search-box').slideDown('fast',function() {$('input[type=text]', this).focus()}); }; return false;"><div class="main-text">Search</div></a></li>
-              <li><a href="#"><div class="main-text">About Us</div></a></li>
-            </ul>
-            </nav>
-
+                <nav id="secondary-menu" class="span2">
+                  <ul class="clearfix">
+                    <li><a href="#"><div class="main-text nav-link">About Us</div></a></li>
+                  </ul>
+                </nav>
             </div>
-            </div>
-        </div>
-        <!--Search content-->
-        <div id="search-box" >
-          <div class="container clearfix">
-          <div class="full-col">
-          <div id="dialog">
-          
-          <input id="q" name="q" placeholder="Enter a band" type="text" value="Phish">
-          <input id="search-btn" class="search btn primary" name="" type="submit" value="Go">
-          
           </div>
-          </div>
-          </div>
-        </div>
-          <!--Body content-->
-          <div id="map" style="position:absolute; top:60px; bottom:0; width:100%;"></div>
-        </div>        
+        <!--Body content-->
+        <div id="map" style="position:absolute; top:60px; bottom:0; width:100%;"></div>
       </div>
     </div>
-    
-    
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
-    <script src="https://raw.github.com/rdio/jquery.rdio.js/master/jquery.rdio.min.js" ></script>
-        
-    <script src="/js/bootstrap.js"></script>
-    <script src="/js/map.js"></script>
-  
-  </body>
+  </div>
+
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
+  <script src="https://raw.github.com/rdio/jquery.rdio.js/master/jquery.rdio.min.js" ></script>
+
+  <script src="/js/bootstrap.js"></script>
+  <script src="/js/map.js"></script>
+
+</body>
 </html>
--- js/map.js
+++ js/map.js
@@ -33,7 +33,7 @@ function clearLayer(layerName){
 
 function JamBaseSearch(query){
 
-    $.getJSON('/tools/jambase.php',{artist:true,name:query}, 
+    $.getJSON('/tools/jambase.php',{artist:true,name:query},
         function(data) {
             if(typeof data.Artists != 'undefined'){
                 JamBaseEvents(data.Artists[0].Id);
@@ -45,9 +45,9 @@ function JamBaseSearch(query){
 }
 
 function JamBaseEvents(artistId){
-	    $.getJSON('/tools/jambase.php',{events:true,artistId:artistId}, 
+	    $.getJSON('/tools/jambase.php',{events:true,artistId:artistId},
         function(data) {
-            
+
             if(typeof data.Events != 'undefined'){
                 $.each(data.Events,function(){
                 	if(typeof this.Venue != 'undefined')
@@ -64,9 +64,9 @@ function JamBaseEvents(artistId){
 }
 
 function Setlists(query){
-	    $.getJSON('/tools/setlistfm.php',{events:true,name:query}, 
+	    $.getJSON('/tools/setlistfm.php',{events:true,name:query},
         function(data) {
-            
+
             if(typeof data.setlists.setlist != 'undefined'){
                 $.each(data.setlists.setlist,function(){
                 	if(typeof this.venue != 'undefined')
@@ -83,7 +83,7 @@ function Setlists(query){
 }
 
 function addPOI_JB(poi){
-	
+
 	function getDesc(poi){
 	    var html='<p>';
 	    if(typeof poi.Venue.Address != 'undefined'){
@@ -106,29 +106,29 @@ function addPOI_JB(poi){
             'marker-color': '#C79926',
             'marker-symbol':'music',
             'description': getDesc(poi)
-        }        
-    });  
+        }
+    });
 
     $('.poi-list').append('<li>'+getDesc(poi)+'</li>');
 }
 
 function addPOI_SL(poi){
-	
+
 	function getDesc(poi){
 	    var html ='<p>';
 	        html+='<br/>'+poi.venue.city['@name']+', '+poi.venue.city['@stateCode'];
 	    html += '</p>';
-	    
+
 	    if(typeof poi.sets.set != 'undefined'){
 		    $.each(poi.sets.set,function(){
 		    	html += '<ul>';
 		    	html += '<div><b>'+this['@name']+'</b></div>';
 			   $.each(this.song,function(){
 			   	  if(!this.cover)
- 			   	  	  html += '<li>'+this['@name']+' <span class="rdio-play"></span></li>'; 	
+ 			   	  	  html += '<li>'+this['@name']+' <span class="rdio-play"></span></li>';
 			   	  else
-					  html += '<li>'+this['@name']+' <b>*</b> <span class="rdio-play"></span></li>'; 
-			   }); 
+					  html += '<li>'+this['@name']+' <b>*</b> <span class="rdio-play"></span></li>';
+			   });
 			   html += '</ul>';
 		    });
 	    }
@@ -146,19 +146,20 @@ function addPOI_SL(poi){
             'marker-color': '#2554C7',
             'marker-symbol':'music',
             'description': getDesc(poi)
-        }        
-    });  
+        }
+    });
 
     $('.poi-list').append('<li>'+getDesc(poi)+'</li>');
 }
 
 $( document ).ready(function() {
-	
+
     $('#search-btn').on('click',function() {
         if($('#q').val()!=''){
-        	clearLayer();        
+        	clearLayer();
 	        JamBaseSearch($('#q').val());
             Setlists($('#q').val());
+
         }
     });
 });
